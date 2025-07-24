088d7f831cd58f485862be060c2f5d844854dcb3
Sheikh Sohel Moon <33374843+Sohel160202@users.noreply.github.com>
2025-07-25 04:28:03 +0600

    Update VRModel.jsx

--- src/VRModel.jsx
+++ src/VRModel.jsx
@@ -46,7 +46,7 @@ export default function VRModel() {
         if (!hiddenTimers[name]) {
           const timer = setTimeout(() => {
             if (part) part.visible = false;
-          }, 500);
+          }, 100);
 
           setHiddenTimers((prev) => ({ ...prev, [name]: timer }));
         }
