ca5598a327cb4242c126e677798639553d97f92c
Sheikh Sohel Moon <33374843+Sohel160202@users.noreply.github.com>
2025-07-25 03:37:12 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, ScrollControls } from '@react-three/drei';
 import VRModel from './VRModel';
+import InfoPanels from './InfoPanels';
 
 export default function App() {
   return (
     <Canvas 
       camera={{ position: [0, 0, 8], fov: 40 }}
       style={{ height: '100vh', width: '100vw' }}
-      >
+    >
       <ambientLight intensity={0.5} />
       <directionalLight position={[2, 2, 2]} intensity={1.2} />
 
       <ScrollControls pages={1} damping={0.1}>
         <VRModel />
+        <InfoPanels />
       </ScrollControls>
 
       <OrbitControls enableZoom={false} />
