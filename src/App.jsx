919f3c4725be8dfac2d1a02ce260c118b9cfed52
Sheikh Sohel Moon <33374843+Sohel160202@users.noreply.github.com>
2025-07-25 03:30:06 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,12 @@ import VRModel from './VRModel';
 
 export default function App() {
   return (
-    <Canvas camera={{ position: [0, 0, 8], fov: 40 }}>
+    <Canvas 
+      camera={{ position: [0, 0, 8], fov: 40 }}
+      style={{ height: '100vh', width: '100vw' }}
+      >
       <ambientLight intensity={0.5} />
-      <directionalLight position={[10, 10, 5]} intensity={1.5} />
+      <directionalLight position={[2, 2, 2]} intensity={1.2} />
 
       <ScrollControls pages={1} damping={0.1}>
         <VRModel />
