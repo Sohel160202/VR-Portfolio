4b0fca7c4980172e65d303a22dfaa5f9b076968d
Sheikh Sohel Moon <33374843+Sohel160202@users.noreply.github.com>
2025-07-25 04:09:00 +0600

    Update VRModel.jsx

--- src/VRModel.jsx
+++ src/VRModel.jsx
@@ -1,26 +1,14 @@
 import { useGLTF } from '@react-three/drei';
-import { useRef } from 'react';
-import { useFrame } from '@react-three/fiber';
-import { useScroll } from '@react-three/drei';
+import { useRef, useEffect } from 'react';
 
-export default function VRModel(props) {
-  const { scene, nodes } = useGLTF('/Cybernetic_Listener_Splitted.glb');
-  const modelRef = useRef();
-  const scroll = useScroll();
+export default function VRModel({ modelRef }) {
+  const { scene } = useGLTF('/Cybernetic_Listener_Splitted.glb');
 
-  useFrame(() => {
+  useEffect(() => {
     if (modelRef.current) {
-      modelRef.current.rotation.y = scroll.offset * Math.PI * 2;
+      modelRef.current.add(scene);
     }
-  });
+  }, [scene, modelRef]);
 
-  return (
-    <primitive
-      ref={modelRef}
-      object={scene}
-      scale={[2, 2, 2]}
-      position={[0, -0.5, 0]}
-      {...props}
-    />
-  );
+  return null;
 }
