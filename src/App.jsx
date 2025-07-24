be26fe844ea2c796c7f2356c464423aa92e0d8fe
Sheikh Sohel Moon <33374843+Sohel160202@users.noreply.github.com>
2025-07-25 04:21:14 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 // File: src/App.jsx
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, ScrollControls } from '@react-three/drei';
-import { useRef } from 'react';
 import VRModel from './VRModel';
 
 export default function App() {
-  const modelRef = useRef();
-
   return (
     <Canvas camera={{ position: [0, 0, 8], fov: 40 }} style={{ height: '100vh', width: '100vw' }}>
       <ambientLight intensity={0.5} />
       <directionalLight position={[2, 2, 2]} intensity={1.2} />
 
       <ScrollControls pages={5} damping={0.1}>
-        <group ref={modelRef}>
-          <VRModel modelRef={modelRef} />
-        </group>
+        <VRModel />
       </ScrollControls>
 
       <OrbitControls enableZoom={false} />
