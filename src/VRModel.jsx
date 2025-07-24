39277e6acd698cb1226bc576de9e2ffd81948809
Sheikh Sohel Moon <33374843+Sohel160202@users.noreply.github.com>
2025-07-25 02:22:47 +0600

    Update VRModel.jsx

--- src/VRModel.jsx
+++ src/VRModel.jsx
@@ -1,22 +1,25 @@
 import { useGLTF } from '@react-three/drei';
 import { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
+import { useScroll } from '@react-three/drei';
 
 export default function VRModel(props) {
   const { scene } = useGLTF('/Cybernetic_Listener_Splitted.glb');
-  const ref = useRef();
+  const modelRef = useRef();
+  const scroll = useScroll();
 
   useFrame(() => {
-    //ref.current.rotation.y += 0.003;
+    if (modelRef.current) {
+      modelRef.current.rotation.y = scroll.offset * Math.PI * 2; // full 360° rotation based on scroll
+    }
   });
 
   return (
     <primitive
-      ref={ref}
+      ref={modelRef}
       object={scene}
-      scale={[2, 2, 2]}
-      position={[0, -0.5, 0]}
-      rotation={[0, Math.PI, 0]}
+      scale={1.5}
+      position={[0, -1.2, 0]}
       {...props}
     />
   );
