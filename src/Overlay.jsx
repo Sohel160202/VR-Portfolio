a59a9749d26e474c0833b7298e1f8329416a2f89
Sheikh Sohel Moon <33374843+Sohel160202@users.noreply.github.com>
2025-07-25 05:44:56 +0600

    Update Overlay.jsx

--- src/Overlay.jsx
+++ src/Overlay.jsx
@@ -10,7 +10,7 @@ export default function Overlay() {
   const leftExpRef = useRef();
   const rightExpRef = useRef();
 
-  const TOTAL_PAGES = 6;
+  const TOTAL_PAGES = 4;
 
   useFrame(() => {
     const currentPage = Math.floor(scroll.offset * TOTAL_PAGES);
