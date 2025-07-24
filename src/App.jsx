41cc4b7181e0f5caf4ac1746136b666e011b292e
Sheikh Sohel Moon <33374843+Sohel160202@users.noreply.github.com>
2025-07-25 03:22:53 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
+// File: src/App.jsx
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Html } from '@react-three/drei';
+import { OrbitControls, ScrollControls } from '@react-three/drei';
 import VRModel from './VRModel';
-import Hotspots from './Hotspots';
 
 export default function App() {
   return (
-    <Canvas camera={{ position: [0, 1.5, 5], fov: 50 }}>
+    <Canvas camera={{ position: [0, 0, 8], fov: 40 }}>
       <ambientLight intensity={0.5} />
       <directionalLight position={[10, 10, 5]} intensity={1.5} />
-      <VRModel />
-      <Hotspots />
+
+      <ScrollControls pages={1} damping={0.1}>
+        <VRModel />
+      </ScrollControls>
+
       <OrbitControls enableZoom={false} />
     </Canvas>
   );
